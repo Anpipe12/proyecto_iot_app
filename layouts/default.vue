1202ff1a66cf413718a753d3d8a5ca546af8c627
Anpipe12 <85084889+Anpipe12@users.noreply.github.com>
2021-06-28 15:48:37 -0500

    Add files via upload

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,13 +4,13 @@
 
     <side-bar
       :background-color="sidebarBackground"
-      short-title="GL"
-      title="IoTicos GL"
+      short-title="IOT"
+      title="Proyecto IoT"
     >
       <template slot-scope="props" slot="links">
         <sidebar-item
           :link="{
-            name: 'Dashboard',
+            name: 'Panel de Control',
             icon: 'tim-icons icon-laptop',
             path: '/dashboard'
           }"
@@ -19,7 +19,7 @@
 
         <sidebar-item
           :link="{
-            name: 'Devices',
+            name: 'Dispositivos',
             icon: 'tim-icons icon-light-3',
             path: '/devices'
           }"
@@ -28,7 +28,7 @@
 
         <sidebar-item
           :link="{
-            name: 'Alarms',
+            name: 'Alarmas',
             icon: 'tim-icons icon-bell-55',
             path: '/alarms'
           }"
@@ -37,7 +37,7 @@
 
         <sidebar-item
           :link="{
-            name: 'Templates',
+            name: 'Plantillas',
             icon: 'tim-icons icon-atom',
             path: '/templates'
           }"
@@ -101,7 +101,7 @@ export default {
   },
   data() {
     return {
-      sidebarBackground: "primary", //vue|blue|orange|green|red|primary
+      sidebarBackground: "blue", //vue|blue|orange|green|red|primary
       client: null,
       options: {
         host: process.env.mqtt_host,
